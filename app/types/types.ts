fdfeb254d58906185a17da0deaee6bc42d492220
Nigel Bomett <nigelbomett@gmail.com>
2024-07-08 19:00:29 +0300

    added type for Journal Entry model

--- app/types/types.ts
+++ app/types/types.ts
@@ -4,3 +4,12 @@ export type RootStackParamList = {
     Register:undefined;
     Home:undefined;
 };
+
+export type JournalEntry ={
+    id: number;
+    userId: number;
+    title: string;
+    content: string;
+    category: string;
+    date: string;
+}
